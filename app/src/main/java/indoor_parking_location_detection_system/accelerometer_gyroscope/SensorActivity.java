6c4ac2e4c3634a8bc5d90b85b6c7d4abcf163e68
Aon1840 <aonattapon1840.ap@gmail.com>
2018-04-28 18:17:44 +0700

    init project

---
+++ app/src/main/java/indoor_parking_location_detection_system/accelerometer_gyroscope/SensorActivity.java
@@ -0,0 +1,13 @@
+package indoor_parking_location_detection_system.accelerometer_gyroscope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+    }
+}
