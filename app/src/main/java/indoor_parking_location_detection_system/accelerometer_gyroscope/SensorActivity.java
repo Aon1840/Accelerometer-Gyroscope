f5936380e6a9dd874e498b9761aafca540987099
Aon1840 <aonattapon1840.ap@gmail.com>
2018-04-28 18:41:14 +0700

    Add SensorActivity

--- app/src/main/java/indoor_parking_location_detection_system/accelerometer_gyroscope/SensorActivity.java
+++ app/src/main/java/indoor_parking_location_detection_system/accelerometer_gyroscope/SensorActivity.java
@@ -1,13 +1,60 @@
 package indoor_parking_location_detection_system.accelerometer_gyroscope;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SensorActivity extends AppCompatActivity {
+
+    TextView testX,testY,testZ;
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        testX = findViewById(R.id.testX);
+        testY = findViewById(R.id.testY);
+        testZ = findViewById(R.id.testZ);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(accelListener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(accelListener);
+    }
+
+    SensorEventListener accelListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            testX.setText("X : "+(int)x);
+            testY.setText("Y : "+(int)y);
+            testZ.setText("Z : "+(int)z);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
